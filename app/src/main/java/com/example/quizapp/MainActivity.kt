6356183ab904eaa21a32c3c0ad0a130e86e45c4e
Nasser Alawi <duniaalahlam2016@gmail.com>
2021-05-21 21:24:38 +0300

    add navigation drawer

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
@@ -11,22 +12,29 @@ import com.example.quizapp.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+   lateinit var drawerLayout : DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
 
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this , R.layout.activity_main)
 
+  drawerLayout = binding.drawerLayout
 
         val navController = this.findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this ,navController)
+
+        NavigationUI.setupActionBarWithNavController(this ,navController , drawerLayout )
+
+        NavigationUI.setupWithNavController(binding.navView , navController )
 
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController : NavController= this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController , drawerLayout)
     }
 
 }
