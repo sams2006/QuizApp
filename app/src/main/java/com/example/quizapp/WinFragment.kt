ff7ddab5efadc4fd02bdd99f16bfbdf18051c073
Nasser Alawi <duniaalahlam2016@gmail.com>
2021-05-24 00:07:01 +0300

    add share menu and intent to email

--- app/src/main/java/com/example/quizapp/WinFragment.kt
+++ app/src/main/java/com/example/quizapp/WinFragment.kt
@@ -1,10 +1,9 @@
 package com.example.quizapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import com.example.quizapp.databinding.FragmentAboutBinding
@@ -13,6 +12,7 @@ import com.example.quizapp.databinding.FragmentWinBinding
 
 class WinFragment : Fragment() {
 
+    private lateinit var args:WinFragmentArgs
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,12 +21,42 @@ class WinFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentWinBinding = DataBindingUtil.inflate(inflater , R.layout.fragment_win , container , false)
 
+        setHasOptionsMenu(true)
+
         binding.tryAgainButton.setOnClickListener ( Navigation.createNavigateOnClickListener(R.id.action_winFragment_to_questionsFragment) )
-        val args = WinFragmentArgs.fromBundle(requireArguments())
+         args = WinFragmentArgs.fromBundle(requireArguments())
         binding.totalScore.text = args.numCorrect.toString()
         binding.wrongAnswer.text = args.numWrong.toString()
         return binding.root
     }
 
+    private fun getIntent(): Intent {
+        args = WinFragmentArgs.fromBundle(requireArguments())
+        // create new intent
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT , getString(R.string.share_text , args.numWrong , args.numCorrect))
+        return shareIntent
+    }
 
+    private fun sendIntent(){
+
+        startActivity(getIntent())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.share_menu , menu)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.share -> sendIntent()
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
